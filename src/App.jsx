8d4fe93f476c77ae0f1aa3548224a58ad9500648
MEhtishamAnwar <mehtishamanwar52@gmail.com>
2026-06-27 20:41:15 +0500

    Navbar and Hero Section completion

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useState,useEffect } from 'react';
+import Banner from './components/Banner/Banner';
+import Navbar from './components/Navbar/Navbar';
+
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+  
+    const handleOrderPopup = () => {
+      setOrderPopup(!orderPopup);
+    };
+    useEffect(() => {
+      AOS.init({
+        offset: 100,
+        duration: 800,
+        easing: "ease-in-sine",
+        delay: 100,
+      });
+      AOS.refresh();
+    }, []);
+  return (
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200'>
+<Navbar handleOrderPopup={handleOrderPopup} />
+      <Banner/>
+    </div>
+  )
+}
+
+export default App
